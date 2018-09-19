8ea89d6c8f1b4cdac5ab36156e304f5da8a2fd94
Denise Golverdingen <dgolverdingen@competa.com>
2018-09-19 10:24:21 +0200

    Added a JavaScript auto-fill form

---
+++ Javascript form auto fill/auto-fill.js
@@ -0,0 +1,6 @@
+function copyName() {
+	document.getElementById('name2').value = document.getElementById('name1').value;
+	document.getElementById('email2').value = document.getElementById('email').value;
+	document.getElementById('phone2').value = document.getElementById('phone').value;
+	document.getElementById('landen2').value = document.getElementById('landen').value;
+  }
