f90471be4d57e5c55f0ea4d300712090d3dcc694
dgolverdingen12 <dgolverdingen@competa.com>
2018-10-04 14:28:02 +0200

    added tab switcher

--- Jquery/Tab switcher/main.js
+++ Jquery/Tab switcher/main.js
@@ -1,13 +1,8 @@
-function openTab(evt, settings) {
-	var i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
-	}
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
-	}
-	document.getElementById(settings).style.display = "block";
-	evt.currentTarget.className += " active";
-}
+$(document).ready(function(){
+	$(".tab").click(function(){
+		$(".tab").removeClass("active");
+		$(".tab__content").removeClass("show");
+		$(this).addClass("active");
+		$($(this).attr("href")).addClass("show");
+	});
+});
