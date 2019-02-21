8c76c58dab3004c4a6f7f1fd4155c8da76b55255
dgolverdingen12 <dgolverdingen@competa.com>
2019-02-21 23:26:42 +0100

    Changed the folder structure

--- Jquery/Tab switcher/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$(".tab").click(function(){
-		$(".tab").removeClass("active");
-		$(".tab__content").removeClass("show");
-		$(this).addClass("active");
-		$($(this).attr("href")).addClass("show");
-	});
-});
--- Jquery/Tab switcher/readme.md
+++
@@ -1,1 +0,0 @@
-*Run gulp watch*
